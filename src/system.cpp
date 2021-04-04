e1f548de864ef1ede33aee607d132864c0a33779
madan <madan.rajendran@gmail.com>
2021-04-03 21:45:21 -0500

    OS working

--- src/system.cpp
+++ src/system.cpp
@@ -24,10 +24,10 @@ vector<Process>& System::Processes() { return processes_; }
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
